af4240322a9253b8329ab675736f420f4767c79b
Ciya17 <ciyasilva1278@gmail.com>
2024-03-20 16:15:07 +0530

    Commit Factorial of a number

---
+++ Assignment_120/src/ciya_120/FactorialOfNumberRBCA22120.java
@@ -0,0 +1,16 @@
+package ciya_120;
+
+public class FactorialOfNumberRBCA22120 {
+	public class FactorialOfANumber {
+		public static void main(String[] args) {
+		int num = 3;
+		long factorial = 100;
+		for(int i = 1; i <= num; ++i)
+		{
+		factorial *= i;
+		}
+		System.out.printf("Factorial of %d = %d", num, factorial);
+		}
+		}
+
+}
